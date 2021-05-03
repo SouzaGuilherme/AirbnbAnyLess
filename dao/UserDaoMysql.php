a38ea623a6fa5d37b90e381ca5151dd8d608e0ef
thiagoheron <thiagoheronavila@gmail.com>
2021-05-03 20:40:04 -0300

    Adicionando métodos de add, edit, remove do UserDaoMysql

--- dao/UserDaoMysql.php
+++ dao/UserDaoMysql.php
@@ -1,12 +1,12 @@
 <?php
-require_once 'models/User.php';
+require_once '../models/User.php';
+
 
 class UserDaoMysql implements UserDAO {
     private $pdo;
 
     public function __construct(PDO $pdo) {
         $this->pdo = $pdo;
-        
     }
 
     private function generateUser($dictData) {
@@ -38,5 +38,47 @@ class UserDaoMysql implements UserDAO {
            }
        }
        return False;
+    }
+
+    public function add(User $user) {
+        $sql = $this->pdo->prepare("INSERT INTO usuarios (cpf, numero_seq_end, codigo_cidade, uf, nome, email, telefone, foto, tipo_usuario, senha, token) VALUES (:cpf, :numero_seq_end, :codigo_cidade, :uf, :nome, :email, :telefone, :foto, :tipo_usuario, :senha, :token);");
+
+        $sql->bindValue(":cpf", $user->getCpf());
+        $sql->bindValue(":numero_seq_end", $user->getNumeroSeqEnd());
+        $sql->bindValue(":codigo_cidade", $user->getCodigoCidade());
+        $sql->bindValue(":uf", $user->getUf());
+        $sql->bindValue(":nome", $user->getNome());
+        $sql->bindValue(":email", $user->getEmail());
+        $sql->bindValue(":telefone", $user->getTelefone());
+        $sql->bindValue(":foto", $user->getFoto());
+        $sql->bindValue(":tipo_usuario", $user->getTipoUsuario());
+        $sql->bindValue(":senha", $user->getSenha());
+        $sql->bindValue(":token", $user->getToken());
+        $sql->execute();
+    }
+
+    public function remove(User $user){
+        $sql = $this->pdo->prepare("DELETE FROM usuarios WHERE cpf = :cpf");
+        $sql->bindValue(":cpf", $user->getCpf());
+        $sql->execute();
+    }
+
+    public function edit(User $user){
+
+        $sql = $this->pdo->prepare("UPDATE usuarios (cpf, numero_seq_end, codigo_cidade, :uf, nome, email, telefone, foto, tipo_usuario, senha, token) VALUES ( :cpf, :numero_seq_end, :codigo_cidade, :uf, :nome, :email, :telefone, :foto, :tipo_usuario, :senha, :token) WHERE cpf = :cpf;");
+
+        $sql->bindValue(":cpf", $user->getCpf());
+        $sql->bindValue(":numero_seq_end", $user->getCpf());
+        $sql->bindValue(":codigo_cidade", $user->getCpf());
+        $sql->bindValue(":uf", $user->getCpf());
+        $sql->bindValue(":nome", $user->getCpf());
+        $sql->bindValue(":email", $user->getCpf());
+        $sql->bindValue(":telefone", $user->getCpf());
+        $sql->bindValue(":foto", $user->getCpf());
+        $sql->bindValue(":tipo_usuario", $user->getCpf());
+        $sql->bindValue(":senha", $user->getCpf());
+        $sql->bindValue(":token", $user->getCpf());
+        $sql->execute();
+
     }
 }
