6cd2bfa47efbde39689ee8ad2ce9fed00f96b393
thiagoheron <thiagoheronavila@gmail.com>
2021-05-08 19:48:02 -0300

    Configura login no index.php

--- dao/UserDaoMysql.php
+++ dao/UserDaoMysql.php
@@ -1,8 +1,8 @@
 <?php
-require_once '../models/User.php';
+require_once 'models/Usuario.php';
 
 
-class UserDaoMysql implements UserDAO {
+class UsuarioDaoMysql implements UsuarioDAO {
     private $pdo;
 
     public function __construct(PDO $pdo) {
@@ -10,19 +10,36 @@ class UserDaoMysql implements UserDAO {
     }
 
     private function generateUser($dictData) {
-        return new User(
-            $dictData['cpf'],
-            $dictData['numeroSeqEnd'],
-            $dictData['codigoCidade'],
-            $dictData['uf'],
-            $dictData['nome'],
-            $dictData['email'],
-            $dictData['telefone'],
-            $dictData['foto'],
-            $dictData['tipoUsuario'],
-            $dictData['senha'],
-            $dictData['token'],
-        );
+
+        if ($dictData['tipo_usuario'] == "LOCATARIO") {
+            return new Locatario(
+                $dictData['cpf'],
+                $dictData['numero_seq_end'],
+                $dictData['codigo_cidade'],
+                $dictData['uf'],
+                $dictData['nome'],
+                $dictData['email'],
+                $dictData['telefone'],
+                $dictData['foto'],
+                $dictData['tipo_usuario'],
+                $dictData['senha'],
+                $dictData['token'],
+            );
+        elseif ($dictData['tipo_usuario'] == "PROPRIETARIO") {
+            return new Proprietario(
+                $dictData['cpf'],
+                $dictData['numero_seq_end'],
+                $dictData['codigo_cidade'],
+                $dictData['uf'],
+                $dictData['nome'],
+                $dictData['email'],
+                $dictData['telefone'],
+                $dictData['foto'],
+                $dictData['tipo_usuario'],
+                $dictData['senha'],
+                $dictData['token'],
+            );
+        } 
     }
 
     public function findByToken($token) {
@@ -37,11 +54,33 @@ class UserDaoMysql implements UserDAO {
                 return $user;
            }
        }
-       return False;
+       return false;
     }
 
-    public function add(User $user) {
-        $sql = $this->pdo->prepare("INSERT INTO usuarios (cpf, numero_seq_end, codigo_cidade, uf, nome, email, telefone, foto, tipo_usuario, senha, token) VALUES (:cpf, :numero_seq_end, :codigo_cidade, :uf, :nome, :email, :telefone, :foto, :tipo_usuario, :senha, :token);");
+    public function findByEmail($email) { 
+
+        if (!empty($email)){
+
+            $sql = $this->pdo->prepare("SELECT * FROM usuarios WHERE email = :email");
+            $sql->bindValue(":email", $email);
+            $sql->execute();
+
+            if ($sql->rowCount() > 0){
+                $dictData = $sql->fetch(PDO::FETCH_ASSOC);
+                $user = $this->generateUser($dictData);
+                $SESSION["flash"] .= "Encontrou Email-";
+                return $user;
+            }
+        }
+        return false;
+    }
+
+    public function add(Usuario $user) {
+        $sql = $this->pdo->prepare("INSERT INTO usuarios (
+            cpf, numero_seq_end, codigo_cidade, uf, nome, email, telefone, foto, tipo_usuario, senha, token
+        ) VALUES (
+            :cpf, :numero_seq_end, :codigo_cidade, :uf, :nome, :email, :telefone, :foto, :tipo_usuario, :senha, :token
+        );");
 
         $sql->bindValue(":cpf", $user->getCpf());
         $sql->bindValue(":numero_seq_end", $user->getNumeroSeqEnd());
@@ -57,28 +96,42 @@ class UserDaoMysql implements UserDAO {
         $sql->execute();
     }
 
-    public function remove(User $user){
+    public function remove(Usuario $user){
         $sql = $this->pdo->prepare("DELETE FROM usuarios WHERE cpf = :cpf");
         $sql->bindValue(":cpf", $user->getCpf());
         $sql->execute();
     }
 
-    public function edit(User $user){
+    public function update(Usuario $user){
 
-        $sql = $this->pdo->prepare("UPDATE usuarios (cpf, numero_seq_end, codigo_cidade, :uf, nome, email, telefone, foto, tipo_usuario, senha, token) VALUES ( :cpf, :numero_seq_end, :codigo_cidade, :uf, :nome, :email, :telefone, :foto, :tipo_usuario, :senha, :token) WHERE cpf = :cpf;");
+        $sql = $this->pdo->prepare("UPDATE usuarios SET
+            cpf = :cpf,
+            numero_seq_end = :numero_seq_end,
+            senha = :senha,
+            nome = :nome,
+            codigo_cidade = :codigo_cidade,
+            uf = :uf,
+            telefone = :telefone,
+            email = :email,
+            foto = :foto,
+            tipo_usuario = :tipo_usuario,
+            token = :token
+            WHERE cpf = :cpf;"
+        );
 
         $sql->bindValue(":cpf", $user->getCpf());
-        $sql->bindValue(":numero_seq_end", $user->getCpf());
-        $sql->bindValue(":codigo_cidade", $user->getCpf());
-        $sql->bindValue(":uf", $user->getCpf());
-        $sql->bindValue(":nome", $user->getCpf());
-        $sql->bindValue(":email", $user->getCpf());
-        $sql->bindValue(":telefone", $user->getCpf());
-        $sql->bindValue(":foto", $user->getCpf());
-        $sql->bindValue(":tipo_usuario", $user->getCpf());
-        $sql->bindValue(":senha", $user->getCpf());
-        $sql->bindValue(":token", $user->getCpf());
+        $sql->bindValue(":numero_seq_end", $user->getNumeroSeqEnd());
+        $sql->bindValue(":codigo_cidade", $user->getCodigoCidade());
+        $sql->bindValue(":uf", $user->getUf());
+        $sql->bindValue(":nome", $user->getNome());
+        $sql->bindValue(":email", $user->getEmail());
+        $sql->bindValue(":telefone", $user->getTelefone());
+        $sql->bindValue(":foto", $user->getFoto());
+        $sql->bindValue(":tipo_usuario", $user->getTipoUsuario());
+        $sql->bindValue(":senha", $user->getSenha());
+        $sql->bindValue(":token", $user->getToken());
         $sql->execute();
 
+        return true;
     }
-}
+}   
--- index.php
+++ index.php
@@ -3,8 +3,6 @@
 require 'config.php';
 require 'models/Auth.php';
 
-define('ROOT_PATH', dirname(__FILE__));
-
 $auth = new Auth($pdo, $base_url);
 
 $userInfo = $auth->checkToken();
