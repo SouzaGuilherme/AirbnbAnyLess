336b7214e16da1775fc9ad0c8aeeb390933aaa2e
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2021-06-16 23:02:25 -0300

    ajustando

--- pages/cadastrar_usuario.php
+++ pages/cadastrar_usuario.php
@@ -13,11 +13,15 @@ require_once __DIR__ . '/../config.php';
     <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
     <link rel="icon" type="image/png" href="<?= $base_url; ?>/assets/images/favicon-32x32.png" />
     <link rel="stylesheet" href="<?= $base_url; ?>/assets/css/login.css" />
+    <link rel="stylesheet" href="<?= $base_url; ?>/../assets/pages/header_register.css" />
     
 </head>
 
-<body>
-    <div class="containers-main">
+<body class="containers-main">
+
+    <?php require_once __DIR__ . '/../assets/pages/header_register.php' ?>
+
+    <div>
             <form method="POST" action="<?= $base_url; ?>/pages/actions/cadastrar_usuario_action.php">
 
                 <input required placeholder="Nome" class="input" type="nome" name="nome" />
