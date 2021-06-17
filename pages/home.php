bd174d863710743499b6a9890b344bfdba74c9f7
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2021-06-16 21:29:41 -0300

    add more codes

--- pages/home.php
+++ pages/home.php
@@ -1,36 +1,27 @@
 <?php
-    require_once __DIR__ . '/../config.php';
+require_once __DIR__ . '/../config.php';
 ?>
-
 
 <!DOCTYPE html>
 <html>
+
 <head>
     <meta charset="utf-8" />
-    <title>HOME</title>
-    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1"/>
-    <link rel="icon" type="image/png" href="<?=$base_url;?>/assets/images/favicon-32x32.png"/>
-    <link rel="stylesheet" href="<?=$base_url;?>/assets/css/login.css" />
+    <title>Bem-vindo ao AirbnbAnyLess</title>
+    <meta name="viewport" content="width=device-width,minimum-scale=1,initial-scale=1" />
+    <link rel="stylesheet" href="<?= $base_url; ?>/assets/css/login.css" />
+    <link rel="stylesheet" href="<?= $base_url; ?>/assets/css/home.css" />
+    <link rel="stylesheet" href="<?= $base_url; ?>/assets/pages/header_application.css" />
+    <link rel="stylesheet" href="<?= $base_url; ?>/assets/pages/find.css" />
+
 </head>
-<body>
-    <header>
-        <div class="container">
-            <a href="<?=$base_url;?>"><img src="<?=$base_url;?>/assets/images/logo.png" /></a>
-        </div>
-    </header>
+
+<body class="bg">
+
     
-    <section class="container main">
-        <form method="POST" action="<?=$base_url;?>/pages/cadastrar_imovel.php">
-            <input class="button" type="submit" value="Cadastrar Imóvel" />
-        </form>
-
-
-        <form method="POST" action="<?=$base_url;?>/pages/visualizar_imoveis.php">
-            <input class="button" type="submit" value="Visualizar Todos Imóveis" />
-        </form>
-    </section>
-
-
+    <?php require_once __DIR__ . '/../assets/pages/header_application.php' ?>
+    <?php require_once __DIR__ . '/../assets/pages/find.php' ?>
 
 </body>
+
 </html>
