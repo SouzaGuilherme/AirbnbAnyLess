43989816032b724b9d1ef6463af0669b98c78c8c
Kevin Oliveira <kevinoliveiracc14@gmail.com>
2021-06-18 00:16:46 -0300

    mais ajustes

--- assets/pages/find.php
+++ assets/pages/find.php
@@ -7,8 +7,8 @@
 
         <input type="text" class="city" placeholder="Cidade">
         <input type="text" class="country" placeholder="País">
-        <input type="date" class="start-date">
-        <input type="date" class="end-date">
+        <input type="text" class="start-date" placeholder="Check-in">
+        <input type="text" class="end-date" placeholder="Check-out">
         <input type="text" class="people" placeholder="Nº de Quartos">
         <input type="text" class="price" placeholder="Preço">
         <input type="submit" class="find" value="Procurar"></button>
--- pages/home.php
+++ pages/home.php
@@ -13,14 +13,39 @@ require_once __DIR__ . '/../config.php';
     <link rel="stylesheet" href="<?= $base_url; ?>/assets/css/home.css" />
     <link rel="stylesheet" href="<?= $base_url; ?>/assets/pages/header_application.css" />
     <link rel="stylesheet" href="<?= $base_url; ?>/assets/pages/find.css" />
+    <link rel="stylesheet" href="<?= $base_url; ?>/assets/pages/footer.css" />
 
 </head>
 
 <body class="bg">
 
-    
     <?php require_once __DIR__ . '/../assets/pages/header_application.php' ?>
     <?php require_once __DIR__ . '/../assets/pages/find.php' ?>
+
+    <p class="line typing-animation">Encontre qualquer lugar para ficar!</p>
+
+    <div class="imovel-container">
+        
+        <h1 class="title-city-container">Algumas cidades para você!</h1>
+        
+        <img src="/../assets/images/rio.jpg" class="city1">
+        <div class="middle">
+            <div class="text">Rio de Janeiro</div>
+        </div>
+
+        <img src="/../assets/images/porto2.jpeg" class="city2">
+        <div class="middle">
+            <div class="text">Porto Alegre</div>
+        </div>
+
+        <img src="/../assets/images/belo.jpg" class="city3">
+        <div class="middle">
+            <div class="text">Belo Horizonte</div>
+        </div>
+        <?php require_once __DIR__ . '/../assets/pages/footer.php' ?>
+    </div>
+
+    
 
 </body>
 
