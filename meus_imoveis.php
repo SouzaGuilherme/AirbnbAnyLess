e795e1994e9a7d275393183acdeeddf61973ef56
thiagoheron <thiagoheronavila@gmail.com>
2021-06-18 09:11:15 -0300

    Adicionando página de meus_imoveis

---
+++ meus_imoveis.php
@@ -0,0 +1,72 @@
+<?php require 'pages/header.php'; ?>
+<?php
+if (empty($_SESSION['cLogin'])) {
+?>
+    <script type="text/javascript">
+        window.location.href = "login.php";
+    </script>
+<?php
+    exit;
+}
+?>
+<div class="container">
+    <h1>Meus Imóveis</h1>
+
+    <a href="add_imovel.php" class="btn btn-default">Adicionar Imóvel</a>
+
+    <table class="table table-striped">
+        <thead>
+            <tr>
+                <th>Foto</th>
+                <th>Código</th>
+                <th>Valor</th>
+                <th>UF</th>
+                <th>Qtd. Quartos</th>
+                <th>Qtd. Banheiros</th>
+                <th>Qtd. Salas</th>
+                <th>Piscina</th>
+                <th>Vagas Garagem</th>
+                <th>Habilitado</th>
+            </tr>
+        </thead>
+
+
+        <?php
+        require 'dao/ImovelDaoMysql.php';
+        $imovelDaoMysql = new ImovelDaoMysql($pdo);
+
+        $imoveis_by_cpf = $imovelDaoMysql->findAllImoveisByCpf($_SESSION['cLogin']);
+
+        foreach ($imoveis_by_cpf as $imovel) :
+        ?>
+            <tr>
+                <td>
+
+                    <?php if (!empty($imovel['url'])) : ?>
+                        <img src="assets/images/imoveis/<?php echo $imovel['url']; ?>" height="50" border="0" />
+                    <?php else : ?>
+                        <img src="assets/images/default.jpg" height="50" border="0" />
+                    <?php endif; ?>
+                </td>
+
+                <td><?php echo $imovel['codigo_imovel']; ?></td>
+                <td>R$ <?php echo number_format($imovel['valor'], 2); ?></td>
+                <td><?php echo $imovel['uf']; ?></td>
+                <td><?php echo $imovel['qtd_quartos']; ?></td>
+                <td><?php echo $imovel['qtd_banheiros']; ?></td>
+                <td><?php echo $imovel['qtd_salas']; ?></td>
+                <td><?php echo $imovel['piscina']; ?></td>
+                <td><?php echo $imovel['vagas_garagem']; ?></td>
+                <td><?php echo $imovel['habilitado']; ?></td>
+
+                
+
+                <td>
+                    <a href="editar-anuncio.php?id=<?php echo $imovel['id']; ?>" class="btn btn-default">Editar</a>
+                    <a href="excluir-anuncio.php?id=<?php echo $imovel['id']; ?>" class="btn btn-danger">Excluir</a>
+                </td>
+            </tr>
+        <?php endforeach; ?>
+    </table>
+</div>
+<?php require 'pages/footer.php'; ?>
