10fc035c83c940e639eb6c89d484d94df9d71259
skileh <heliksmm@hotmail.com>
2021-06-19 02:44:21 -0300

    qeijo

--- pages/home.php
+++ pages/home.php
@@ -1,10 +1,9 @@
 <?php
 require_once __DIR__ . '/../config.php';
 
-if(!isset($_SESSION["token"]))
-{
-header("Location: login.php");
-exit;
+if (!isset($_SESSION["token"])) {
+    header("Location: login.php");
+    exit;
 }
 
 ?>
@@ -27,28 +26,25 @@ exit;
 <body class="bg">
 
     <?php require_once __DIR__ . '/../assets/pages/header_application.php' ?>
-    <?php require_once __DIR__ . '/../assets/pages/find.php' ?>
+    <div class="options-component">
 
-    <p class="line typing-animation">Encontre qualquer lugar para ficar!</p>
-
-    <div class="imovel-container">
-        
-        <h1 class="title-city-container">Algumas cidades para você!</h1>
-        
-        <img src="/../assets/images/rio.jpg" class="city1">
-        <!--span class="image-text">Rio de Janeiro</span-->
-    
-        <img src="/../assets/images/porto2.jpeg" class="city2">
-        <!--span class="image-text">Porto Alegre</span-->
-
-        <img src="/../assets/images/belo.jpg" class="city3">
-        <!--span class="image-text">Belo Horizonte</span-->
-
-        <?php require_once __DIR__ . '/../assets/pages/footer.php' ?>
+        <input type="text" class="city" placeholder="Cidade">
+        <input type="text" class="country" placeholder="País">
+        <input type="text" class="start-date" placeholder="Check-in">
+        <input type="text" class="end-date" placeholder="Check-out">
+        <input type="text" class="people" placeholder="Nº de Quartos">
+        <input type="text" class="price" placeholder="Preço">
+        <input type="submit" class="find" value="Procurar">
 
     </div>
 
-    
+
+    <?php require_once __DIR__ . '/../assets/pages/home_images.php' ?>
+
+    <?php require_once __DIR__ . '/../assets/pages/footer.php' ?>
+
+
+
 
 </body>
 
