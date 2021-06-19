8ef3ae5eb2dbf82687e6873c8f1af694ebdb981b
thiagoheron <thiagoheronavila@gmail.com>
2021-06-18 21:36:51 -0300

    Adiciona findBycCodeCity e findByAllCity na DaoCity

--- dao/CidadeDaoMysql.php
+++ dao/CidadeDaoMysql.php
@@ -29,6 +29,26 @@ class CidadeDaoMysql implements CidadeDAO {
         return false;
     }
 
+    public function findByCodeCity($codigo_cidade) {
+        
+        $sql = $this->pdo->prepare("SELECT * FROM cidades WHERE codigo_cidade = :codigo_cidade ORDER BY uf, nome ASC");
+        $sql->bindValue(":codigo_cidade", $codigo_cidade);
+        $sql->execute();
+
+        $cidade = [];
+        if ($sql->rowCount() > 0){
+            $dictData = $sql->fetch();
+            $cidade = new Cidade(
+                $dictData['codigo_cidade'],
+                $dictData['uf'],
+                $dictData['nome'],
+            );
+            return $cidade;
+        }
+        return false;
+        
+    }
+
     public function findAllCity() {
         
         $sql = $this->pdo->prepare("SELECT * FROM cidades ORDER BY uf, nome ASC");
@@ -36,7 +56,14 @@ class CidadeDaoMysql implements CidadeDAO {
 
         $array = [];
         if ($sql->rowCount() > 0){
-            $array = $sql->fetchAll();
+            $dataAll = $sql->fetchAll();
+            foreach ($dataAll as $data) {
+                array_push($array, new Cidade(
+                    $data['codigo_cidade'],
+                    $data['uf'],
+                    $data['nome'],
+                ));
+            }
         }
         return $array;
         
